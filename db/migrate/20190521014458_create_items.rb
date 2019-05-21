e518f08f39fdfa9ea1288728b2e5a6922f1ffc2a
grisadiche <grisadiche@gmail.com>
2019-05-20 21:07:17 -0500

    initial commit

---
+++ db/migrate/20190521014458_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :manufacturer
+      t.string :model
+      t.integer :price
+      t.integer :weight
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
