8fd3a549833ec3ab3aa3ad44dfe7b53d163f5432
grisadiche <grisadiche@gmail.com>
2019-06-17 19:37:51 -0500

    application.rb cleaned up

--- spec/api_constraints_spec.rb
+++ spec/api_constraints_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe ApiConstraints do
   let(:api_constraints_v1) { ApiConstraints.new(version: 1) }
-  let(:api_constraints_v2) { ApiConstraints.new(version: 2, default: true) }
 
   describe "matches?" do
 
