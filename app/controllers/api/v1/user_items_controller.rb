ec9109ddc050989a372c1aa24526a3a03730eae1
Dania Simmons <dania02525@gmail.com>
2019-07-21 20:32:07 -0600

    post base64 enc image to api

--- app/controllers/api/v1/user_items_controller.rb
+++ app/controllers/api/v1/user_items_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class UserItemsController < AuthenticatedController
-      # protect_from_forgery with: :null_session, if: Proc.new {|c| c.request.format.json? }
-
       def index
         render json: {
           status: :ok,
@@ -17,20 +15,17 @@ module Api
         }
       end
 
-      def new
-      end
-
       def create
-        @new_item = Item.new
-        @new_item = current_user.items.build(safe_params)
-          if @new_item.save
-            render json: {
-              status: :created,
-            }
-          else
-            render json: {
-              error: "not_found"
-            }, status: :not_found
+        @new_item = current_user.items.build(safe_params.except(:image))
+        convert_and_attach_image!
+        if @new_item.save
+          render json: {
+            data: Api::V1::ItemPresenter.new(@new_item).present
+          }, status: :created
+        else
+          render json: {
+            error: @new_item.errors.full_messages.join(', ')
+          }, status: :unprocessable_entity
         end
       end
 
@@ -39,8 +34,27 @@ module Api
 
       private
 
+      def convert_and_attach_image!
+        # delete the image from the params
+        blob = safe_params.delete(:image)
+        return unless blob.present?
+        @new_item.image.attach(
+          io: StringIO.new(Base64.decode64(blob)),
+          filename: 'my_image.jpg',
+          content_type: 'image/png'
+        )
+      end
+
       def safe_params
-        params.require(:user_item).permit(:manufacturer, :model, :weight, :price, :description, :serial_number, :color, :image) #add :image with base64 encoding
+        @_safe_params ||=
+          params.require(:user_item).permit(:manufacturer,
+                                            :model,
+                                            :weight,
+                                            :price,
+                                            :description,
+                                            :serial_number,
+                                            :color,
+                                            :image)
       end
     end
   end
